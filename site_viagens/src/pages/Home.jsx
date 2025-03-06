712af77eea1ac8dcd4f799234b417beeeefb9e24
Laura Marques <lauramarques00000@gmail.com>
2025-03-06 13:28:36 -0300

    Laura_Footer_06/03

--- site_viagens/src/pages/Home.jsx
+++ site_viagens/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import './Home.css'
 import Header from "../components/Header"
+import Footer2 from '../components/FooterProps2'
 
 function Home() {
   return (
-    <div>
+    <>
       <Header/>
-    </div>
+      <Footer2
+      texto1='Direitos reservador de 
+      Grupo5 SESI SENAI vinhedo Estado de SP
+       @Institutodaeducação - Aprovação de direitos de imagem 
+       nacional Brasileiro Mundial 🙂'
+      texto2='Mais uma vez, muito obrigado(a) por escolher a nossa empresa. Sua confiança é o nosso maior patrimônio.
+Atenciosamente,Grupo 5 😉'
+      />
+    </>
   )
 }
 
