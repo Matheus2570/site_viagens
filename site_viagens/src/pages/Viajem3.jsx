d4f5c71ec01ea1d676418ecab791cf8669546936
Laura Marques <lauramarques00000@gmail.com>
2025-03-12 09:38:26 -0300

    Laura_12/03_viagem3

--- site_viagens/src/pages/Viajem3.jsx
+++ site_viagens/src/pages/Viajem3.jsx
@@ -20,10 +20,21 @@ function Viajem3() {
 
       <CardDentro 
   nome={imagem1} 
-  descricao="Descrição detalhada do lugar" 
-  dataedisponibilidade="Disponível até 31/12/2025" 
+  descricao= {
+    <>
+    As Maldivas são uma nação tropical no Oceano Índico composta por 26 atóis em formato de anel, 
+    que são formados por mais de 1.000 ilhas de coral. 
+    São 14 h 30 min para chegar (é necessário pegar barco)<br></br><br></br>
+    PACOTE IMPERDÍVEL <br></br>
+    Até 2 pessoas  <br></br>
+    5 diárias <br></br>
+    Incluso: Café da manhã, Almoço, Café da tarde, Janta, Área de lazer, Guia turístico por 5 horas diárias, Cancelamento gratuito. <br></br>
+    Valor a vista:  R$8.493 <br></br>
+    Ou até 3 x R$2.831 sem juros!!!<br></br>
+    </>
+  } 
 />
-
+ 
 <ProximaViajem texto="Próximo" caminho="/viajem4" />
       <Footer/>
     </>
