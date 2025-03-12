d9be192702bf106bcd0ccbd32495e34770b9ff07
Laura Marques <lauramarques00000@gmail.com>
2025-03-12 09:53:32 -0300

    Laura/Ana_12/03_viajem2

--- site_viagens/src/pages/Viajem2.jsx
+++ site_viagens/src/pages/Viajem2.jsx
@@ -20,9 +20,20 @@ function Viajem2() {
 
       <CardDentro 
   nome={imagem1} 
-  descricao="Descrição detalhada do lugar" 
-  dataedisponibilidade="Disponível até 31/12/2025" 
-/>
+  descricao={
+    <>
+   Florença, capital da região Toscana, na Itália, abriga muitas obras de arte e arquitetura renascentistas. São 11 h 50 min para chegar.<br></br><br></br>
+   PACOTE IMPERDÍVEL<br></br> 
+   Até 6 pessoas  <br></br>
+   8 diárias <br></br>
+   Incluso: Café da manhã, Almoço, Café da tarde, Janta, Área de lazer, Guia turístico por 5 horas diárias, Cancelamento gratuito. <br></br>
+   Valor a vista: R$7798,00, ou até 5 x R$1.559,00 sem juros!!! <br></br>
+   Disponível até: 07/06/2025
+    </>
+  } 
+/> 
+ 
+
 
 <ProximaViajem texto="Próximo" caminho="/viajem3" />
       <Footer/>
