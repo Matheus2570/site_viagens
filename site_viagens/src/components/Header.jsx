474d933eae7ad39487c8cb83201284a6ea12292c
Maria Clara Freire <mariaclaracgfreire@gmail.com>
2025-03-12 08:32:28 -0300

    Dia 12/03

--- site_viagens/src/components/Header.jsx
+++ site_viagens/src/components/Header.jsx
@@ -1,12 +1,11 @@
 import './Header.css'
-import logo from "../assets/Logo-grupo5.png";
+
 
 function Header() {
   return (
     <>
  <header className="header">
-        <h1>Viagens Grupo 5</h1>
-        <img src={logo} className="logo" alt="Imgem React" />
+        <h1>Five Tourists ✈</h1>
       </header>
   
     </>
